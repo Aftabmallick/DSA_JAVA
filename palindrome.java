379419f06d29fe3773327a3baacf01031da02109
Aftabmallick <aftabmallick000@gmail.com>
2023-03-24 12:41:57 +0530

    new file:   palindrome.class modified:   palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -2,7 +2,21 @@ import java.util.Scanner;
 
 public class palindrome {
     public static void main(String[] args) {
+        int reverse = 0;
+        int last_digit;
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Enter  no: ");
+        int number = sc.nextInt();
+        int num=number;
+        while (number > 0) {
+            last_digit = number % 10;
+            reverse = reverse * 10 + last_digit;
+            number = number / 10;
+        }
+        if (num == reverse) {
+            System.out.println(num + " is a Palindrome no");
+        } else {
+            System.out.println(num + " is not a Palindrome no");
+        }
     }
 }
